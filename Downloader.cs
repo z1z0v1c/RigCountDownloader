7fdd2ff86eac781d8e5a066aa132b4f96102989e
z1z0v1c <zizovic.aleksandar@outlook.com>
2023-08-21 14:48:28 +0200

    Add Downloader class

---
+++ Downloader.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using HtmlAgilityPack;
+
+namespace RigCountDownloader
+{
+	internal class Downloader
+	{
+		private static readonly string Url = "https://bakerhughesrigcount.gcs-web.com/intl-rig-count?c=79687&p=irol-rigcountsintl";
+
+		private readonly HttpClient _httpClient;
+
+		public Downloader()
+		{
+			_httpClient = new HttpClient();
+			_httpClient.DefaultRequestHeaders.Add("User-Agent", "RigCountDownloader/1.0");
+			_httpClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
+		}
+
+		public async Task<HtmlDocument> GetHtmlDocumentAsync()
+		{
+			try
+			{
+				string htmlContent = await GetHtmlContentAsync();
+				return LoadHtml(htmlContent);
+			}
+			catch (Exception ex)
+			{
+				Trace.TraceError("An exception occurred: " + ex.Message);
+				Trace.TraceError("Stack Trace: " + ex.StackTrace);
+				return new HtmlDocument();
+			}
+		}
+
+		private async Task<string> GetHtmlContentAsync()
+		{
+			try
+			{
+				HttpResponseMessage response = await _httpClient.GetAsync(Url);
+				if (response.IsSuccessStatusCode)
+				{
+					return await response.Content.ReadAsStringAsync();
+				}
+				else
+				{
+					Console.WriteLine($"Failed to get content. Status code: {response.StatusCode}");
+					return string.Empty;
+				}
+			}
+			catch (Exception ex)
+			{
+				Trace.TraceError("An exception occurred: " + ex.Message);
+				Trace.TraceError("Stack Trace: " + ex.StackTrace);
+				return string.Empty;
+			}
+		}
+
+		private HtmlDocument LoadHtml(string htmlContent)
+		{
+			var htmlDocument = new HtmlDocument();
+			htmlDocument.LoadHtml(htmlContent);
+			return htmlDocument;
+		}
+	}
+}
