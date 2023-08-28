dde647a931c8c1d23f1b3d8415a8e03bd6c54164
z1z0v1c <zizovic.aleksandar@outlook.com>
2023-08-28 14:54:13 +0200

    Add ExcelStreamProcessorTests

---
+++ RigCountDownloader.Tests/StreamProcessors/ExcelStreamProcessorTests.cs
@@ -0,0 +1,35 @@
+﻿using NSubstitute;
+using RigCountDownloader.FileConverters;
+using RigCountDownloader.StreamProcessors;
+using Xunit;
+
+namespace RigCountDownloader.Tests
+{
+	public class ExcelStreamProcessorTests
+	{
+		private readonly IFileConverterFactory fileConverterFactory;
+		private readonly IFileConverter fileConverter;
+
+		public ExcelStreamProcessorTests()
+		{
+			fileConverterFactory = Substitute.For<IFileConverterFactory>();
+			fileConverter = Substitute.For<ExcelFileConverter>();
+		}
+
+		[Fact]
+		public async Task ProcessStreamAsync_CorrectMemoryStream_CallsFileConverter()
+		{
+			// Arrange
+			fileConverterFactory.CreateFileConverter().Returns(fileConverter);
+			var excelStreamProcessor = new ExcelStreamProcessor(fileConverterFactory);
+			using var memoryStream = new MemoryStream();
+
+			// Act
+			await excelStreamProcessor.ProcessStreamAsync(memoryStream);
+
+			// Assert
+			fileConverterFactory.Received(1).CreateFileConverter();
+			await fileConverter.Received(1).ConvertAndSaveAsync();
+		}
+	}
+}
