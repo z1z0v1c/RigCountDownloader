9e8370a476806b89377d35ec53f72fc47859452d
Aleksandar Zizovic <zizovic.aleksandar@outlook.com>
2025-04-17 22:30:59 +0200

    Configure http client to fully mimic a browser

--- RigCountDownloader/StreamDownloader.cs
+++ RigCountDownloader/StreamDownloader.cs
@@ -14,8 +14,33 @@ namespace RigCountDownloader
 			_logger = logger;
 			_configuration = configuration;
 			_httpClient = httpClient;
-			_httpClient.DefaultRequestHeaders.Add("User-Agent", "RigCountDownloader/1.0");
-			_httpClient.DefaultRequestHeaders.Add("Connection", "keep-alive");
+
+			ConfigureHttpClient();
+		}
+
+		// Configure http client to fully mimic a browser
+		private void ConfigureHttpClient()
+		{
+			_httpClient.DefaultRequestHeaders.Clear();
+
+			_httpClient.DefaultRequestHeaders.Add(
+				name: "User-Agent",
+				value: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+			);
+			_httpClient.DefaultRequestHeaders.Add(
+				name: "Accept",
+				value: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
+			);
+			_httpClient.DefaultRequestHeaders.Add("Accept-Language", "en-US,en;q=0.9");
+			_httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+			_httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "document");
+			_httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
+			_httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Site", "none");
+			_httpClient.DefaultRequestHeaders.Add("Sec-Fetch-User", "?1");
+			_httpClient.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+			_httpClient.DefaultRequestHeaders.Add("Cache-Control", "max-age=0");
+
+			_httpClient.Timeout = TimeSpan.FromMinutes(5);
 		}
 
 		public async Task<Stream> DownloadFileAsStreamAsync()
